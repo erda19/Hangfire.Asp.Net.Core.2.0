ef37d31c8baee05944d1eaa73eab5441e08c0f6f
erda19 <erda.puradinata@gmail.com>
2018-10-27 21:01:16 +0700

    my first commit
    my first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aaa.Models;
+using Hangfire;
+
+namespace aaa.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            BackgroundJob.Enqueue(() =>  testJob());
+            return View();
+        }
+
+        public void testJob()
+        {
+             Console.WriteLine("Fire-and-forget!");
+        }
+
+        public IActionResult Contact()
+        {
+            RecurringJob.AddOrUpdate(
+                                        () => Console.WriteLine("Recurring!"),
+                                        Cron.MinuteInterval(1));
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using Hangfire.MySql.Core;
+
+namespace aaa
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(config => 
+                config.UseStorage(
+                    new MySqlStorage("Server=localhost;Database=hangfire-test;Uid=root;Pwd=erda19;SslMode=none;Allow User Variables=true")
+                ));
+            
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the dotnetHTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+            
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
